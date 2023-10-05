826993293d3adb61c1a60c9af6e24f95d4ee89de
Baris Yerlikaya <b.yerlikaya@outlook.com>
2023-10-05 14:32:11 +0300

    Sample api project added.

---
+++ sample/Sample.Api/ApplicationSpecific/Contexts/MemoryDbContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Sample.Common.Entity;
+using System.Reflection;
+
+namespace Sample.Api.ApplicationSpecific.Contexts
+{
+    public class MemoryDbContext : DbContext
+    {
+        public DbSet<Publisher> Publishers { get; set; }
+
+        public DbSet<Book> Books { get; set; }
+
+        public DbSet<Author> Authors { get; set; }
+
+        public DbSet<Country> Countries { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                base.OnConfiguring(optionsBuilder.UseInMemoryDatabase("DummyDataDb"));
+            }
+        }
+    }
+}
---
+++ sample/Sample.Api/Controllers/PublisherController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Sample.Api.ApplicationSpecific.Contexts;
+using Sample.Common.Dto;
+using Sample.Common.Entity;
+using SmartOrderBy;
+using SmartOrderBy.Extensions;
+
+namespace Sample.Api.Controllers
+{
+    [ApiController]
+    public class PublisherController : ControllerBase
+    {
+        private readonly MemoryDbContext _context;
+
+        public PublisherController(MemoryDbContext context)
+        {
+            _context = context;
+
+            OrderByMapper.Map<Publisher, Book>("bookId", x => x.Id);
+            OrderByMapper.Map<Publisher, Book, Author>("authorAge", x => x.Age);
+            OrderByMapper.Map<Publisher, Book, Author, Country>("countryName", x => x.Name);
+            OrderByMapper.Map<Publisher, Author, Language>("lanId", x => x.Id);
+        }
+
+        [HttpPost("/publishers")]
+        public IActionResult GetPublishers(PublisherRequest request)
+        {
+            var result = _context.Publishers
+                .Include(x => x.Books)
+                .ThenInclude(x => x.Author)
+                .OrderBy(request.OrderBy)
+                .ToList();
+
+            return Ok(result);
+        }
+
+        [HttpPost("/books")]
+        public IActionResult GetBooks(BookSearchRequest request)
+        {
+            var result = _context.Books
+                .Include(x => x.Author)
+                .OrderBy(request.OrderBy)
+                .ToList();
+
+            return Ok(result);
+        }
+    }
+}
